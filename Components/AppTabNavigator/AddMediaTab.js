ce40c37133e7adae4fb6ca7361e3a881f2c8aa0f
junhyun87 <pe.jeon87@gmail.com>
2019-07-24 00:13:12 +0900

    alarm test1

--- Components/AppTabNavigator/AddMediaTab.js
+++ Components/AppTabNavigator/AddMediaTab.js
@@ -31,164 +31,158 @@ class AddMediaTab extends Component {
     )
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>LikesTab</Text>
-      </View>
-    );
-  }
 
-  // constructor(props) {
-  //     super(props),
-  //     this.state = {
-  //       currentTime: "25:00",
-  //       workTime: "25:00",
-  //       breakTime: "05:00",
-  //       working: true,
-  //       timer: null,
-  //       paused: false,
-  //       playing: false,
-  //     }
-  //     this.setWorkTimer = this.setWorkTimer.bind(this);
-  //     this.setBreakTimer = this.setBreakTimer.bind(this);
-  //     this.playButton = this.playButton.bind(this);
-  //     this.pauseButton = this.pauseButton.bind(this);
-  //     this.resetButton = this.resetButton.bind(this);
-  //     this.countdown = this.countdown.bind(this);
-  //     this.toggleStatus = this.toggleStatus.bind(this);
-  //   }
 
-  //   setWorkTimer(val) {
-  //     let newTime = getTime(val);
-  //     this.setState({
-  //       workTime: newTime,
-  //     });
-  //     if (!this.state.timer) {
-  //       this.setState({
-  //         currentTime: newTime,
-  //       });
-  //     }      
-  //   }
+  constructor(props) {
+      super(props),
+      this.state = {
+        currentTime: "25:00",
+        workTime: "25:00",
+        breakTime: "05:00",
+        working: true,
+        timer: null,
+        paused: false,
+        playing: false,
+      }
+      this.setWorkTimer = this.setWorkTimer.bind(this);
+      this.setBreakTimer = this.setBreakTimer.bind(this);
+      this.playButton = this.playButton.bind(this);
+      this.pauseButton = this.pauseButton.bind(this);
+      this.resetButton = this.resetButton.bind(this);
+      this.countdown = this.countdown.bind(this);
+      this.toggleStatus = this.toggleStatus.bind(this);
+    }
 
-  //   setBreakTimer(val) {
-  //     let newTime = getTime(val);
-  //     this.setState({
-  //       breakTime: newTime,
-  //     });
-  //   }
+    setWorkTimer(val) {
+      let newTime = getTime(val);
+      this.setState({
+        workTime: newTime,
+      });
+      if (!this.state.timer) {
+        this.setState({
+          currentTime: newTime,
+        });
+      }      
+    }
 
-  //   playButton() {
-  //     if (this.state.paused === true || this.state.playing === false) { 
-  //       this.setState({
-  //         timer: setInterval(this.countdown, 1000),
-  //         paused: false,
-  //         playing: true,
-  //       });
-  //     }
-  //   }
+    setBreakTimer(val) {
+      let newTime = getTime(val);
+      this.setState({
+        breakTime: newTime,
+      });
+    }
 
-  //   pauseButton () {
-  //     if (this.state.paused === false && this.state.playing === true) {
-  //       clearInterval(this.state.timer);
-  //       this.setState({
-  //         paused: true,
-  //         timer: null,
-  //         playing: false
-  //       });
-  //       console.log(this.state.paused);
-  //     } else if (this.state.paused === true && this.state.playing === false) {
-  //       this.playButton();
-  //     }       
-  //   }
+    playButton() {
+      if (this.state.paused === true || this.state.playing === false) { 
+        this.setState({
+          timer: setInterval(this.countdown, 1000),
+          paused: false,
+          playing: true,
+        });
+      }
+    }
 
-  //   resetButton () {
-  //     this.pauseButton();
-  //     this.setState({
-  //       currentTime: this.state.workTime,
-  //       playing: false,
-  //       paused: false,
-  //       working: true,
-  //       timer: null
-  //     })
-  //   }
+    pauseButton () {
+      if (this.state.paused === false && this.state.playing === true) {
+        clearInterval(this.state.timer);
+        this.setState({
+          paused: true,
+          timer: null,
+          playing: false
+        });
+        console.log(this.state.paused);
+      } else if (this.state.paused === true && this.state.playing === false) {
+        this.playButton();
+      }       
+    }
 
-  //   countdown() {
-  //     if (this.state.currentTime === "00:00" && this.state.playing === true) {
-  //       console.log('finished');
-  //       vibrate();
-  //       this.toggleStatus();
-  //     } else {
-  //       let sec = this.state.currentTime.slice(3);
-  //       let min = this.state.currentTime.slice(0, 2);
-  //       if (sec === "00") {
-  //         let newMin = leftPadding(parseInt(min) - 1);
-  //         let newTime = newMin + ":59";
-  //         this.setState({
-  //           currentTime: newTime,
-  //         });
-  //       } else {
-  //         let newSec = leftPadding((parseInt(sec) - 1));
-  //         let newTime = min + ":" + newSec;
-  //         this.setState({
-  //           currentTime: newTime,
-  //         })
-  //       }
-  //     }
-  //   }
+    resetButton () {
+      this.pauseButton();
+      this.setState({
+        currentTime: this.state.workTime,
+        playing: false,
+        paused: false,
+        working: true,
+        timer: null
+      })
+    }
 
-  //   toggleStatus() {
-  //     if (this.state.working) {
-  //       this.setState({
-  //         working: false,
-  //         currentTime: this.state.breakTime,
-  //       })
-  //     } else {
-  //       this.setState({
-  //         working: true,
-  //         currentTime: this.state.workTime,
-  //       })
-  //     }
-  //   }
+    countdown() {
+      if (this.state.currentTime === "00:00" && this.state.playing === true) {
+        console.log('finished');
+        vibrate();
+        this.toggleStatus();
+      } else {
+        let sec = this.state.currentTime.slice(3);
+        let min = this.state.currentTime.slice(0, 2);
+        if (sec === "00") {
+          let newMin = leftPadding(parseInt(min) - 1);
+          let newTime = newMin + ":59";
+          this.setState({
+            currentTime: newTime,
+          });
+        } else {
+          let newSec = leftPadding((parseInt(sec) - 1));
+          let newTime = min + ":" + newSec;
+          this.setState({
+            currentTime: newTime,
+          })
+        }
+      }
+    }
 
-  //   render() {
-  //     return (
-  //       <View style={styles.container}>
-  //         <Timer currentTime={this.state.currentTime}/>
-  //         <Label working={this.state.working} paused={this.state.paused} playing={this.state.playing}/>
-  //         <View style={{flexDirection: 'row'}}>
-  //           <Buttons 
-  //             title="Play" 
-  //             onPress={this.playButton} 
-  //           />
-  //           <Buttons 
-  //             title="Pause" 
-  //             onPress={this.pauseButton} 
-  //           />
-  //           <Buttons 
-  //             title="Reset" 
-  //             onPress={this.resetButton} 
-  //           />
-  //         </View>
-  //         <View style={styles.menuContainer}>
-  //           <Text style={styles.menuLabel}>Set work time (min): </Text>
-  //           <Menu 
-  //             selected={Number(this.state.workTime.slice(0, 2)).toString()}
-  //             onValueChange={this.setWorkTimer}
-  //           />
-  //         </View>
-  //         <View style={styles.menuContainer}>
-  //           <Text style={styles.menuLabel}>Set break time (min): </Text>
-  //           <Menu 
-  //             selected={Number(this.state.breakTime.slice(0, 2)).toString()}
-  //             onValueChange={this.setBreakTimer}
-  //           />
-  //         </View> 
-  //         <Info />
-  //         <Picture />
-  //       </View>
-  //     );
-  //   }
+    toggleStatus() {
+      if (this.state.working) {
+        this.setState({
+          working: false,
+          currentTime: this.state.breakTime,
+        })
+      } else {
+        this.setState({
+          working: true,
+          currentTime: this.state.workTime,
+        })
+      }
+    }
+
+    render() {
+      return (
+        <View style={styles.container}>
+          <Timer currentTime={this.state.currentTime}/>
+          <Label working={this.state.working} paused={this.state.paused} playing={this.state.playing}/>
+          <View style={{flexDirection: 'row'}}>
+            <Buttons 
+              title="Play" 
+              onPress={this.playButton} 
+            />
+            <Buttons 
+              title="Pause" 
+              onPress={this.pauseButton} 
+            />
+            <Buttons 
+              title="Reset" 
+              onPress={this.resetButton} 
+            />
+          </View>
+          <View style={styles.menuContainer}>
+            <Text style={styles.menuLabel}>Set work time (min): </Text>
+            <Menu 
+              selected={Number(this.state.workTime.slice(0, 2)).toString()}
+              onValueChange={this.setWorkTimer}
+            />
+          </View>
+          <View style={styles.menuContainer}>
+            <Text style={styles.menuLabel}>Set break time (min): </Text>
+            <Menu 
+              selected={Number(this.state.breakTime.slice(0, 2)).toString()}
+              onValueChange={this.setBreakTimer}
+            />
+          </View> 
+          <Info />
+          <Picture />
+        </View>
+      );
+    }
 }
 
 export default AddMediaTab;
